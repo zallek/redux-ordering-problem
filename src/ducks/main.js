7739a6525eed78e2200c7370f9a76471cfa352a4
Nicolas Fortin <nicolas.fortin37@gmail.com>
2016-04-15 10:57:26 +0200

    Fix async issue
    That's a simple solution to update state only if relevant.
    
    An other solution would be to namespace items in the store by their type as made in the example http://redux.js.org/docs/advanced/ExampleRedditAPI.html (where posts are namespaced by subreddit). Which allow to have nice caching possibility:
    - When the user select 'Fruits' you first check if we already have them in store at `items.fruits`
     - if not you request the API at update the `items[type]` without worrying about conflicts. 
     - otherwise you can directly display items.

--- src/ducks/main.js
+++ src/ducks/main.js
@@ -1,6 +1,7 @@
 // DEFAULT STATE
 const initialState = {
   loading: false,
+  requestingItemType: null,
   items: [],
 }
 
@@ -16,13 +17,16 @@ export default function reducer(state = initialState, { type, payload, meta }) {
     case REQUEST_ITEMS:
       return {
         ...state,
+        requestingItemType: payload.itemType,
         loading: true,
       }
 
     case RECEIVED_ITEMS:
+      if (payload.itemType !== state.requestingItemType) return state;
       return {
         ...state,
         loading: false,
+        requestingItemType: null,
         items: payload.items,
       }
 
@@ -37,6 +41,9 @@ export function getItems(itemType) {
   return (dispatch, getState) => {
     dispatch({
       type: REQUEST_ITEMS,
+      payload: {
+        itemType,
+      },
     })
     // simulate an API call...
     const requestDuration = (itemType === 'fruits' ? 5000 : 200)
@@ -45,6 +52,7 @@ export function getItems(itemType) {
         dispatch({
           type: RECEIVED_ITEMS,
           payload: {
+            itemType,
             items: [
               { type: 'Fruits', name: 'Oranges', stock: 34, desc: 'meh' },
               { type: 'Fruits', name: 'Bananas', stock: 43, desc: 'foo' },
@@ -55,6 +63,7 @@ export function getItems(itemType) {
         dispatch({
           type: RECEIVED_ITEMS,
           payload: {
+            itemType,
             items: [
               { type: 'Vegetables', name: 'Potatoes', stock: 57, desc: 'meh' },
               { type: 'Vegetables', name: 'Carrots', stock: 123, desc: 'foo' },
